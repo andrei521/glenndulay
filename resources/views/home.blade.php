077901a537ecc4cba5ff98aa10330d006889a52c
andrei521 <franz.abello@yahoo.com>
2018-09-11 11:36:50 +0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>glenndulay photography | Digital Portfolio</title>
+	<meta charset="utf-8">
+	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+	<meta name="description" content="Online Digital Portfolio of Glenn Wesley Dulay. My photo collection consists shots I have taken from my various encounters from people to places. Here, you will witness the transition of my love for the craft; from events coverage to commision, glamour, fun shoot and others. Enjoy.">
+	<meta name="keywords" content="glenn dulay, glenndulay, Glenn Dulay, Bahrain , Photographer, middle east, Glenn Wesley Dulay, bahrain, Freelance Photographer, freelance, free lance, Photographer, Food Photography, Architectural, Establishment, Street, Street Portrait, Fashion, Glamour, Wedding, Prenup">
+	<meta name="author" content="Glenn Wesley A. Dulay">
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/touchTouch.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/kwicks-slider.css" type="text/css" media="screen">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/superfish.js"></script>
+	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+	<script type="text/javascript" src="js/jquery.kwicks-1.5.1.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="js/jquery.cookie.js"></script>    
+	<script type="text/javascript" src="js/touchTouch.jquery.js"></script>
+	<script type="text/javascript">if($(window).width()>1024){document.write("<"+"script src='js/jquery.preloader.js'></"+"script>");}	</script>
+
+  <script>    
+     jQuery(window).load(function() { 
+     $x = $(window).width();    
+  if($x > 1024)
+  {     
+  jQuery("#content .row").preloader();    } 
+     
+     jQuery('.magnifier').touchTouch();     
+    jQuery('.spinner').animate({'opacity':0},1000,'easeOutCubic',function (){jQuery(this).css('display','none')});  
+        }); 
+        
+  </script>
+
+	<!--[if lt IE 8]>
+  		<div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/img/upgrade.jpg"border="0"alt=""/></a></div>  
+ 	<![endif]-->
+	<!--[if (gt IE 9)|!(IE)]><!-->
+	<!--<![endif]-->
+	<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <link rel="stylesheet" href="css/docs.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet' type='text/css'>
+  <![endif]-->
+	</head>
+
+	<body>
+<div class="spinner"></div> 
+<!--============================== header =================================-->
+<header>
+      <div class="container clearfix">
+    <div class="row">
+          <div class="span12">
+        <div class="navbar navbar_">
+              <div class="container">
+            <h1 class="brand brand_"><a href="index.html"><img alt="" src="img/logo.png"> </a></h1>
+            <a class="btn btn-navbar btn-navbar_" data-toggle="collapse" data-target=".nav-collapse_">Menu <span class="icon-bar"></span> </a>
+            <div class="nav-collapse nav-collapse_  collapse">
+                  <ul class="nav sf-menu">
+                <li class="active"><a href="index.html">About</a></li>
+                <li class="sub-menu"><a href="folio.html">Folio</a>
+                      <ul>
+                    <li><a href="fg.html">Fashion | Glamour </a></li>
+                    <li><a href="Cars.html">Cars | Vehicles </a></li>
+                    <li><a href="food.html">Food</a></li>
+                    <li><a href="ae.html">Architectural</a></li>
+                    <li><a href="pw.html">Prenuptials | Wedding</a></li>
+                    <li><a href="el.html">Events | Launches</a></li>
+                    <li><a href="street.html">Street Portrait</a></li>
+                    <li><a href="landscape.html">Landscape</a></li>
+                  </ul>
+                    </li>
+                <li><a href="contact.html">Contact</a></li>
+              </ul>
+                </div>
+          </div>
+            </div>
+      </div>
+        </div>
+  </div>
+    </header>
+<div class="bg-content">
+      <div class="container">
+    <div class="row">
+          <div class="span12"> 
+        <!--============================== slider =================================-->
+        <div class="flexslider">
+              <ul class="slides">
+            <li> <img src="slider/pic10.jpg" alt="" > </li>  
+            <li> <img src="slider/pic01.jpg" alt="" > </li>
+            <li> <img src="slider/pic00.jpg" alt="" > </li>
+             <li> <img src="slider/pic11.jpg" alt="" > </li>
+            <li> <img src="slider/pic02.jpg" alt="" > </li>
+            <li> <img src="slider/pic03.jpg" alt="" > </li>
+            <li> <img src="slider/pic04.jpg" alt="" > </li>
+            <li> <img src="slider/pic05.jpg" alt="" > </li>
+            <li> <img src="slider/pic06.jpg" alt="" > </li>
+            <li> <img src="slider/pic07.jpg" alt="" > </li>
+            <li> <img src="slider/pic08.jpg" alt="" > </li>
+            <li> <img src="slider/pic09.jpg" alt="" > </li>
+            <li> <img src="slider/pic017.jpg" alt="" > </li>
+           
+            
+          </ul>
+            </div>
+        <span id="responsiveFlag"></span>
+        <div class="block-slogan">
+              <h2>Achievements</h2>
+              <div>
+            <p><br>
+			  - Bahrain This Week  Contributor 2014 | 2015 | 2016 | 2017<em><a href="http://www.bahrainthisweek.com/category/photography/" target="_blank">(Click here)</a></em><br>
+			  - Sigma , Manfrotto and Fomex Lighting Brand Ambassador on behalf of Yaquby Stores Bahrain 2015 | 2016 | 2017			  <br>
+			  - Top Wedding Photographer in WeddingSociety 2015 | 2016 <a href="https://weddingphotographersociety.com/profiles/photographers/584" target="_blank"><em>(Click Here)</em></a><br>
+			  - Ironman 70.3 Bahrain 2015 | Finisher Pix Official Photographer<br>
+			  - Photo Award ME 2015 Runner up <em>(Market Scene Category) </em><br>
+			  - Manila Bulletin Picture Perfect | Featured Artist February 2015 <a href="http://www.mb.com.ph/going-places/" target="_blank"><em>(Click Here)</em></a><br>
+              - Filipino Life Magazine Inaugural Issue | Featured Artist January 2015 <a href="http://filipinolifemagazine.com/hobbyist-to-professional-photographer/" target="_blank"><em>(Click Here)</em></a><br>
+              - Fujifilm Bahrain Endorser 2014 <br>
+              - Vogue Italy Contributor  ( Vogue.it )<br>
+              </p>
+            <p>- Exhibitor in Al Ali Art Festival 2012 | 2013 | 2014<br>
+              - 2013 MEETUPBH Top 3 in DSLR Photo Competition <br>
+              - Won 2013 BATELCO Bahrain Eye Photography Competition (Landscape Category)</p>
+            <p>- 1st Runner up in Flick a Trip Bahrain Photo Competition 2013<br>
+              - 2nd Runner up GEANT Bahrain Photo Competition 2013 (Landscape Category)<br>
+              - Shortlisted in Bahrain World Trade Center Photo Competition 2013<br>
+              - Won Best of Bahrain Talent Competition 2012 <br>
+              - Won Middle East &amp; Africa Lumix Photo Competition (PINK THEME) October 2012<br>
+              - Featured in ABS-CBN &quot;Naimbag nga Morning | May 2012&quot;<br>
+              - Getty Images Contributor<br>
+              - Exhibitor in Sino Si Juan 2012<br>
+              - ILLUSTRADO Life web magazine’s January 2012 issue<br>
+              - Top 10 Shortlisted in Victorinox Challenge 2011<br>
+              - SHOUT Magazine ( Bahrain ) August 2010 issue<br>
+              - Won FACT Magazine &amp; Yaquby (Bahrain) 2010 &quot; NEW BAHRAIN &quot;</p>
+          </div>
+            </div>
+      </div>
+        </div>
+  </div>
+      
+      <!--============================== content =================================-->
+      
+      <div id="content" class="content-extra"><div class="ic"></div>
+    <div class="row-1">
+          <div class="container">
+        <div class="row"> </div><center>
+          <h5><br>
+            C R E D E N T I A L S<br>
+            <br>
+            
+            <img src="img/credentials.jpg"><br>
+          </h5>
+        </center>
+      </div>
+        </div>
+    <div class="container">
+          <div class="row">
+           <article class="span6">
+              <h3>Glenn Wesley A. Dulay</h3>
+              <div class="wrapper">
+            <figure class="img-indent"><img src="img/glenndulay01.jpg" alt="" /></figure>
+            <div class="inner-1 overflow extra">
+              <div class="txt-1">Glenndulay is a freelance commercial and wedding photographer based in Manama, Kingdom of Bahrain. I have been in the photography for almost 6 years now. I don’t categorize my self as a professional photographer, I would say I am an experienced photographer; my various experiences in the field is what has contributed to my development as of today.</div>
+              He has been sought after for a couple of events coverage and he prefers to do portrait , fashion and wedding.
+              <div class="border-horiz"></div>
+              <div class="overflow">
+                <ul class="list list-pad">
+                      
+                      
+                </ul>
+                <ul class="list">
+                      
+                     
+                </ul>
+              </div>
+                </div>
+          </div>
+            </article>
+          
+            <article class="span6">
+              <h3>Latest photoshoots</h3>
+              <ul class="list-photo">
+            <li><a href="Latestshoot/big/01.jpg" class="magnifier" ><img src="Latestshoot/01.jpg " alt="" /></a></li>
+            <li><a href="Latestshoot/big/02.jpg" class="magnifier" ><img src="Latestshoot/02.jpg " alt="" /></a></li>
+            <li><a href="Latestshoot/big/03.jpg" class="magnifier" ><img src="Latestshoot/03.jpg " alt="" /></a></li>
+            <li class="last"><a href="Latestshoot/big/04.jpg" class="magnifier" ><img src="Latestshoot/04.jpg " alt="" /></a></li>
+            <li><a href="Latestshoot/big/05.jpg" class="magnifier" ><img src="Latestshoot/05.jpg " alt="" /></a></li>
+            <li><a href="Latestshoot/big/06.jpg" class="magnifier" ><img src="Latestshoot/06.jpg " alt="" /></a></li>
+            <li><a href="Latestshoot/big/07.jpg" class="magnifier" ><img src="Latestshoot/07.jpg " alt="" /></a></li>
+            <li class="last"><a href="Latestshoot/big/08.jpg" class="magnifier" ><img src="Latestshoot/08.jpg " alt="" /></a></li>
+            <li><a href="Latestshoot/big/09.jpg" class="magnifier" ><img src="Latestshoot/09.jpg " alt="" /></a></li>
+            <li><a href="Latestshoot/big/10.jpg" class="magnifier" ><img src="Latestshoot/10.jpg " alt="" /></a></li>
+              <li><a href="Latestshoot/big/11.jpg" class="magnifier" ><img src="Latestshoot/11.jpg " alt="" /></a></li>
+            <li><a href="Latestshoot/big/12.jpg" class="magnifier" ><img src="Latestshoot/12.jpg " alt="" /></a></li>
+          </ul>
+            </article>
+      </div>
+        </div>
+  </div>
+    </div>
+
+<!--============================== footer =================================-->
+<footer>
+      <div class="container clearfix">
+    <ul class="list-social pull-right">
+          <li><a class="icon-1" href="https://www.facebook.com/pages/Glenndulay-Photography/228265417215796"></a></li>
+          <li><a class="icon-2" href="http://www.flickr.com/photos/glennwesley"></a></li>
+          <li><a class="icon-3" href="http://www.twitter.com/glenndulay"></a></li>
+         
+        </ul>
+    <div class="privacy pull-left">Copyright © 2017 glenndulay. Photographs & Contents in this website. All Rights Reserved</a> </div>
+  </div>
+    </footer>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+
+</body>
+</html>
